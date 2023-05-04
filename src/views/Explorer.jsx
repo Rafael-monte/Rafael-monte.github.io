a6ca339da1c5f8f8fb2732b1b7b5ac5677b5c4cf
Rafael-monteiro <rafaelmonte46@gmail.com>
2023-05-04 11:04:58 -0300

    States added

--- src/views/Explorer.jsx
+++ src/views/Explorer.jsx
@@ -4,20 +4,63 @@ import './Explorer.css';
 import SearchBar from "../components/search-bar/SearchBar";
 import info from '../data/info.json'
 import FolderIcon from "../components/icon/FolderIcon";
+import FileIcon from "../components/icon/FileIcon";
 export default class Explorer extends Component {
 
+
+    state = {
+        folders: [],
+        infoFolders: []
+    }
+
     getInfos = () => {
-        let folders = info.folders;
-        let asJSX = folders.map((el, idx) => {
+        let _folders = info.elements;
+        let asJSX = _folders.map((el, idx) => {
+            if (el.type === "file") {
+                return (
+                    <li key={idx} onClick={evt => this.changeOtherFolderColors(el.name)}>
+                        <FileIcon folderName={el.name} content={el.content} bgColor={"inherit"}></FileIcon>
+                    </li>
+                )
+            }
             return (
-                <li key={idx}>
-                    <FolderIcon folderName={el.folderName} content={el.content}></FolderIcon>
+                <li key={idx} onClick={evt => this.changeOtherFolderColors(el.name)}>
+                    <FolderIcon folderName={el.name} content={el.content} bgColor={"inherit"}></FolderIcon>
                 </li>
             )
         })
-        return asJSX
+        this.setState({
+            folders: _folders,
+            infoFolders: asJSX
+        });
     }
 
+
+    changeOtherFolderColors = (folderName) => {
+        let _folders = this.state.folders.map((el, idx) => {
+            if (el.type === "file") {
+                return (
+                    <li key={idx} onClick={evt => this.changeOtherFolderColors(el.name)}>
+                            <FileIcon folderName={el.name} content={el.content} 
+                            bgColor={el.name !== folderName? "inherit": "#1338BE"} onClick={evt => this.changeOtherFolderColors(el.name)}></FileIcon>
+                    </li>
+                )
+            }
+           return(
+            <li key={idx} onClick={evt => this.changeOtherFolderColors(el.name)}>
+                    <FolderIcon folderName={el.name} content={el.content} 
+                    bgColor={el.name !== folderName? "inherit": "#1338BE"} onClick={evt => this.changeOtherFolderColors(el.name)}></FolderIcon>
+            </li>
+           ) 
+        })
+        this.setState({
+            infoFolders: _folders
+        });
+    }
+
+    componentDidMount() {
+        this.getInfos();
+    }
 
     render = () => {
         return (
@@ -26,7 +69,7 @@ export default class Explorer extends Component {
                 <SearchBar></SearchBar>
                 <div className="icons">
                     <ul className="icons-list">
-                        {this.getInfos()}
+                        {this.state.infoFolders}
                     </ul>
                 </div>
             </div>
